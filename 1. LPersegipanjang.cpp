270236097e95f831309470aa753faa291f65927e
DanuAdiP <danu261216@gmail.com>
2019-01-22 01:24:03 +0700

    Commit Pertama

---
+++ 1. LPersegipanjang.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int p,l,luas;
+
+cout<<"MENGHITUNG LUAS PERSEGI PANJANG"<<endl<<endl;
+cout<<"Masukkan Panjang = ";cin>>p;
+cout<<"Masukkan Lebar   = ";cin>>l;
+luas=p*l;
+cout<<"Hasil            = "<<luas;
+
+return 0;
+}
